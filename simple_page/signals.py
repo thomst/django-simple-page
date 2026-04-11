f7b535b6cfab718421dff8b3bd8f5a27656abc69
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-11 14:46:40 +0200

    use signals for all orderable items

--- simple_page/signals.py
+++ simple_page/signals.py
@@ -1,33 +1,35 @@
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from django.db import models
-from .models import PageSection
+from .models import PageSection, PageRegion, RegionSection
 
 
-@receiver(pre_save, sender=PageSection)
-def pagesection_pre_save(sender, instance, **kwargs):
+def set_index_on_adding(sender, instance, **kwargs):
     """
-    Signal handler for pre_save on PageSection.
-
-    Validates and prepares the instance before saving.
+    Signal handler for adding orderable items.
     """
     # Ensure order is a valid integer
-    if instance.order is None or instance.order == '':
+    if instance.index is None or instance.index == '':
         page_sections = PageSection.objects.filter(page=instance.page)
-        max_order = page_sections.aggregate(models.Max('order'))['order__max']
-        instance.order = (max_order or -1) + 1
+        instance.add_item(page_sections)
 
 
-@receiver(post_delete, sender=PageSection)
-def pagesection_post_delete(sender, instance, **kwargs):
+pre_save.connect(set_index_on_adding, sender=PageSection)
+pre_save.connect(set_index_on_adding, sender=PageRegion)
+pre_save.connect(set_index_on_adding, sender=RegionSection)
+
+
+def update_indexes_on_deleting(sender, instance, **kwargs):
     """
     Signal handler for post_delete on PageSection.
 
     Reorders remaining sections for the same page after deletion.
     """
     # Reorder remaining sections for this page
-    remaining_sections = PageSection.objects.filter(page=instance.page).order_by('order')
-    for idx, section in enumerate(remaining_sections):
-        if section.order != idx:
-            section.order = idx
-            section.save()
+    remaining_sections = PageSection.objects.filter(page=instance.page)
+    instance.delete_item(remaining_sections)
+
+
+post_delete.connect(update_indexes_on_deleting, sender=PageSection)
+post_delete.connect(update_indexes_on_deleting, sender=PageRegion)
+post_delete.connect(update_indexes_on_deleting, sender=RegionSection)
