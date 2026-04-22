1259f3d3f14d350351c2fc8370714076aa85a764
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-22 11:27:09 +0200

    fix missing region value for dynamically new added inline forms

---
+++ simple_page/static/simple_page/formset_handlers.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('formset:added', (event) => {
+    if (event.detail.formsetName.startsWith('pagesection_set')) {
+        const formset = event.target;
+        const previousFormset = formset.previousElementSibling;
+        const regionName = previousFormset.querySelector('input[name$="-region"]').value;
+        formset.querySelector('input[name$="-region"]').value = regionName;
+    }
+});
