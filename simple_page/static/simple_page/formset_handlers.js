8aae8e47c4488e31eb9e591bbbbfef8b0fbe1c1b
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-26 22:01:19 +0200

    simplify javascript - no need to wait for dom loaded event

--- simple_page/static/simple_page/formset_handlers.js
+++ simple_page/static/simple_page/formset_handlers.js
@@ -1,13 +1,11 @@
 (function() {
     "use strict";
-    document.addEventListener("DOMContentLoaded", function () {
-        document.addEventListener('formset:added', (event) => {
-            if (event.detail.formsetName.startsWith('pagesection_set')) {
-                const formset = event.target;
-                const previousFormset = formset.previousElementSibling;
-                const regionName = previousFormset.querySelector('input[name$="-region"]').value;
-                formset.querySelector('input[name$="-region"]').value = regionName;
-            }
-        });
+    document.addEventListener('formset:added', (event) => {
+        if (event.detail.formsetName.startsWith('pagesection_set')) {
+            const formset = event.target;
+            const previousFormset = formset.previousElementSibling;
+            const regionName = previousFormset.querySelector('input[name$="-region"]').value;
+            formset.querySelector('input[name$="-region"]').value = regionName;
+        }
     });
 })();
