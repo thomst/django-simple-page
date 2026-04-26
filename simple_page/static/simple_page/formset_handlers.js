f432e9178f338b798c701e802cd7054a14f93380
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-26 21:59:09 +0200

    clean javascript scope for the formset-handler

--- simple_page/static/simple_page/formset_handlers.js
+++ simple_page/static/simple_page/formset_handlers.js
@@ -1,9 +1,13 @@
-
-document.addEventListener('formset:added', (event) => {
-    if (event.detail.formsetName.startsWith('pagesection_set')) {
-        const formset = event.target;
-        const previousFormset = formset.previousElementSibling;
-        const regionName = previousFormset.querySelector('input[name$="-region"]').value;
-        formset.querySelector('input[name$="-region"]').value = regionName;
-    }
-});
+(function() {
+    "use strict";
+    document.addEventListener("DOMContentLoaded", function () {
+        document.addEventListener('formset:added', (event) => {
+            if (event.detail.formsetName.startsWith('pagesection_set')) {
+                const formset = event.target;
+                const previousFormset = formset.previousElementSibling;
+                const regionName = previousFormset.querySelector('input[name$="-region"]').value;
+                formset.querySelector('input[name$="-region"]').value = regionName;
+            }
+        });
+    });
+})();
