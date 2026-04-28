1c06c5922a97a446c7515dff46ff6c17c0cd5fe3
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-04-28 09:29:13 +0200

    added verbose-name to app config

--- simple_page/apps.py
+++ simple_page/apps.py
@@ -4,6 +4,8 @@ from django.apps import AppConfig
 class SimplePageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'simple_page'
+    verbose_name = "Simple Page"
+
 
     def ready(self):
         import simple_page.signals
