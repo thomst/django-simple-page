5ad6262496a69a26e90796eab548d1fdc5701722
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-05-18 17:46:35 +0200

    optionally include root page in menu

--- simple_page/templatetags/simple_page.py
+++ simple_page/templatetags/simple_page.py
@@ -4,18 +4,22 @@ register = template.Library()
 
 
 @register.inclusion_tag("simple_page/menu.html")
-def menu(page, max_level=None):
+def menu(page, max_level=None, include_root=False):
     context = dict()
     context['page'] = page
-    context['nodes'] = page.get_root().get_descendants()
     context['max_level'] = max_level
+    context['nodes'] = page.get_root().get_descendants(include_self=include_root)
     return context
 
 
 @register.filter
 def is_active(page, node):
+    root = page.get_root()
     ancestors = page.get_ancestors(include_self=True)
-    return node in ancestors
+    return (
+        node in ancestors and node != root
+        or page == root and node == root
+    )
 
 
 @register.filter
