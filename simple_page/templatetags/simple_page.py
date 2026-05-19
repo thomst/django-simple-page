1a68a192e7915c93f581808536e4c0def1e10beb
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-05-19 10:22:22 +0200

    refactored include_root for menu templatetag

--- simple_page/templatetags/simple_page.py
+++ simple_page/templatetags/simple_page.py
@@ -8,18 +8,16 @@ def menu(page, max_level=None, include_root=False):
     context = dict()
     context['page'] = page
     context['max_level'] = max_level
-    context['nodes'] = page.get_root().get_descendants(include_self=include_root)
+    context['nodes'] = page.get_root().get_descendants()
+    if include_root:
+        context['root'] = page.get_root()
     return context
 
 
 @register.filter
 def is_active(page, node):
-    root = page.get_root()
     ancestors = page.get_ancestors(include_self=True)
-    return (
-        node in ancestors and node != root
-        or page == root and node == root
-    )
+    return node in ancestors
 
 
 @register.filter
