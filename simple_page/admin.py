447789a7918967e0b5abc56b9a3573a22b6ec8aa
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-05-20 17:51:44 +0200

    improved docstrings

--- simple_page/admin.py
+++ simple_page/admin.py
@@ -1,5 +1,18 @@
 """
-TODO
+The admin integration for django-simple-page is realized by a customized page
+modeladmin. This modeladmin is registered for the page model and will be used
+for all proxy page models. It provides the following features:
+
+- Let pages be orderable by drag and drop in the admin changelist view.
+- Let the user choose the type of the page before rendering the page's add form.
+- Set the initial value of the hidden page_type field in the page add form.
+- Render an inline formset for each region of the page which allows to assign
+  sections to the region and rearrange them via drag and drop.
+
+For your own concrete page models you should use :class:`~.admin.BasePageAdmin`
+as a base class for your modeladmin. It will take care of rendering the inline
+formsets for regions and setting the appropriate value for the hidden page_type
+field.
 """
 
 from django.contrib import admin
@@ -15,6 +28,13 @@ from .forms import ReorderRelationForm
 
 
 class BaseRegionInline(admin.TabularInline):
+    """
+    Base inline for page sections. This inline is rendered for each region of a
+    page. It uses a custom form to make the sections orderable via drag and
+    drop within a region. For that we use the django-reorder-items-widget_.
+
+    .. _django-reorder-items-widget: https://github.com/thomst/django-reorder-items-widget
+    """
     region_name = None
     form = ReorderRelationForm
     model = PageSection
@@ -31,8 +51,8 @@ class BaseRegionInline(admin.TabularInline):
 
 class GetPageModelMixin:
     """
-    A mixin to choose the page model based on a page_type url query parameter or
-    the the page_type field of the current page object.
+    A mixin to get the page model based on a page_type url query parameter or
+    the page_type field of the current page object.
     """
 
     @cached_property
@@ -64,8 +84,9 @@ class GetPageModelMixin:
 
 class RenderPageRegionsMixin(GetPageModelMixin):
     """
-    Render a `PageSection` inline formset for each region of the page. Also make
-    sure extra forms have the region's name as initial for the region field.
+    Render a :class:`~.models.PageSection` inline formset for each region of the
+    page. Also make sure extra forms have the region's name as initial value for
+    the region form field.
     """
 
     def get_page_regions(self, request, obj):
@@ -98,8 +119,8 @@ class ChoosePageTypeMixin(GetPageModelMixin):
     """
     Let the user choose the type of the page she wants to add. Therefore render
     a simple list of add-links which either set the page_type url-query
-    parameter for proxy page models. Or use the page's own modeladmin change
-    form for concrete page models.
+    parameter for proxy page models or link to the modeladmin changeform
+    for concrete page models.
     """
 
     def render_change_form(self, request, context, add=False, change=False, form_url="", obj=None):
@@ -130,9 +151,8 @@ class ChoosePageTypeMixin(GetPageModelMixin):
 
 class SetPageTypeMixin(GetPageModelMixin):
     """
-    Be sure that change forms for adding pages have the correct page_type set.
-    The page_type field will be hidden and equipped with an appropriate initial
-    value.
+    Set the initial value of the hidden page_type field in changeforms when
+    adding a new page.
     """
 
     def get_form(self, request, obj=None, **kwargs):
@@ -145,6 +165,16 @@ class SetPageTypeMixin(GetPageModelMixin):
 
 @admin.register(Page)
 class PageAdmin(SetPageTypeMixin, ChoosePageTypeMixin, RenderPageRegionsMixin, DraggableMPTTAdmin):
+    """
+    The modeladmin for all proxy page models. This modeladmin is already
+    registered for the page model. It provides the following features:
+
+    - Let pages be orderable by drag and drop in the admin changelist view.
+    - Let the user choose the type of the page before rendering the page's add
+      form.
+    - Set the initial value of the hidden page_type field in the page add form.
+    - Render an inline formset for each region of the page.
+    """
     list_display = ("tree_actions", "indented_title", "slug", "page_type", "view_page_link")
     list_display_links=('indented_title',)
     search_fields = ("title", "slug")
@@ -159,7 +189,9 @@ class PageAdmin(SetPageTypeMixin, ChoosePageTypeMixin, RenderPageRegionsMixin, D
 
 class BasePageAdmin(SetPageTypeMixin, RenderPageRegionsMixin, admin.ModelAdmin):
     """
-    Use this base class for your own concrete page modeladmin. It will take care
-    of rendering an inline formset for each region. And set the appropriate
-    value for the page_type field.
+    Base class for modeladmins for concrete page models. It provides the
+    following features:
+
+    - Set the initial value of the hidden page_type field in the page add form.
+    - Render an inline formset for each region of the page.
     """
